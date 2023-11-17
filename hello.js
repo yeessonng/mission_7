7c6efeddc5f469516812a3affa2cf7c55dfe5d9e
yeessonng <dorlek856@gmail.com>
2023-11-18 01:12:26 +0900

    first

---
+++ hello.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use((request, response) => {
+    response.send('<h1>Hello express</h1>');
+});
+
+app.listen(52273, () => {
+    console.log('server running at http://127:0.0.1:52273');
+});
+
+//ddd
