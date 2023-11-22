74cce9d53eeef98ecda81fbb316166603000204b
yeessonng <dorlek856@gmail.com>
2023-11-22 16:02:52 +0900

    storeAdd수정

--- src/models/stores.sql.js
+++ src/models/stores.sql.js
@@ -1,4 +1,4 @@
 export const insertStoreSql = "INSERT INTO store (region_id, name, address, check_status) VALUES (?, ?, ?, ?);";
 export const getRegionStoreSql = "SELECT store.name as storeName, region.name as regionName " +
 "FROM store JOIN region ON store.region_id = region.id " +
-"WHERE region.id = ?;";
+"WHERE store.id = ?;";
