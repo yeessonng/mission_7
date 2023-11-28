f4e0920eee91b29f077d4c3979c8beed1c3fe6f3
yeessonng <dorlek856@gmail.com>
2023-11-28 17:54:59 +0900

    가게리뷰조회

--- src/providers/stores.provider.js
+++ src/providers/stores.provider.js
@@ -3,6 +3,6 @@ import {previewReviewResponseDTO} from '../dtos/stores.dto.js';
 
 //가게 리뷰 목록 조회
 export const getReview = async (storeId, query) => {
-    const {reviewId, size = 3} = query;//구조 분해 할당
-    return previewReviewResponseDTO(await getPreviewReview(reviewId, size, storeId))
+    const {reviewId, paging = 3} = query;//구조 분해 할당
+    return previewReviewResponseDTO(await getPreviewReview(reviewId, paging, storeId))
 }
